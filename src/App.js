2fd5bbb791b4c6575051f49c6a06a74bc497fbc9
Lukerdue <jamielynjenks@gmail.com>
2021-01-28 15:05:30 -0700

    started some stuff, but this is too ambitious for a one day project, so I'm going to deploy an old project and work on this one more

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@
 function App() {
   return (
     <div className="App">
+      <div className="title">
+            <h1>Jamie Jenks</h1>
+            <h2>Full Stack web Developer</h2>
+          </div>
     </div>
   );
 }
