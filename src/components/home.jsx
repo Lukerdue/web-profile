8f49eaa352e43f9115696706db9bd0c52c391e2d
Jamie Jenks <jamielynjenks@gmail.com>
2023-02-15 12:41:58 -0700

    instantiated file structure, added a css reset, and started the home page hero section

---
+++ src/components/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HomeStyle } from '../styles/homeStyled';
+
+function Home(){
+    return (<HomeStyle>
+        <div className="titles">
+            <h1>Jamie Jenks</h1>
+            <h2>Web Developer</h2>
+        </div>
+        </HomeStyle>)
+}
+export default Home
---
+++ src/styles/homeStyled.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const HomeStyle = styled.div `
+    height: 100vh;
+    width: 100vw;
+    background-color: #3d3d3d;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    h1{
+        color: white;
+        font-size: 36px;
+        padding: 0;
+        margin: 0;
+    }
+    h2{
+        color: white;
+        font-size: 28px;
+        padding: 0;
+        margin: 0;
+    }
+`
