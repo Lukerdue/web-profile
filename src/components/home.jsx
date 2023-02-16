93ae2804f66d730d6d2959d5a9192b81e824d8bc
Jamie Jenks <jamielynjenks@gmail.com>
2023-02-16 13:01:57 -0700

    I got the moon to a good place now :)

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,10 +3,22 @@ import { HomeStyle } from '../styles/homeStyled';
 
 function Home(){
     return (<HomeStyle>
-        <div className="titles">
-            <h1>Jamie Jenks</h1>
-            <h2>Web Developer</h2>
-        </div>
+            <div className="titles">
+                <h1>Jamie Jenks</h1>
+                <h2>Web Developer</h2>
+            </div>
+            <div className="moonwrapper">
+                <div className="actualmoon">
+                    <div className="crater one"></div>
+                    <div className="crater two"></div>
+                    <div className="crater three"></div>
+                    <div className="stacked">
+                        <div className="crater four"></div>
+                        <div className="crater six"></div>
+                    </div>
+                    <div className="crater five"></div>
+                </div>
+            </div>
         </HomeStyle>)
 }
 export default Home
--- src/styles/homeStyled.jsx
+++ src/styles/homeStyled.jsx
@@ -2,22 +2,77 @@ import React from 'react';
 import styled from 'styled-components';
 
 export const HomeStyle = styled.div `
+    box-sizing: border-box;
     height: 100vh;
     width: 100vw;
-    background-color: #3d3d3d;
+    background-color: #121212;
+    width: 100%;
     display: flex;
     align-items: center;
-    justify-content: center;
-    h1{
-        color: white;
-        font-size: 36px;
-        padding: 0;
+    justify-content: space-between;
+    .titles{
+        width: 30%;
+        display: flex;
+        flex-direction: column;
         margin: 0;
     }
-    h2{
+    .titles > h1, .titles > h2{
         color: white;
-        font-size: 28px;
-        padding: 0;
-        margin: 0;
+        max-width: 100%;
+    }
+    /* moon stuff */
+    .moonwrapper{
+        display: block;
+        width: 50%;
+        height: 100%;
+        padding-top: 10%;
+    }
+    .actualmoon{
+        width: 250px;
+        height: 250px;
+        border-radius: 50%;
+        background-color: #ccd3ff;
+        display: flex;
+        box-shadow: 0 0 120px white;
+    }
+    .crater{
+        border-radius: 50%;
+        background-color: #b2bcf7;
+    }
+    .stacked{
+        display: flex;
+        flex-direction: column;
+        align-items: space-evenly;
+        justify-content: space-evenly;
+    }
+    .one{
+        height: 45px;
+        width: 45px;
+        margin-left: 35px;
+        margin-top: 45px;
+    }
+    .three{
+        height: 15px;
+        width: 15px;
+        margin-top: 15px;
+        margin-left: 20px;
+    }
+    .two{
+        height: 20px;
+        width: 20px;
+        margin-top: 155px;
+    }
+    .four{
+        height: 30px;
+        width: 30px;
+    }
+    .five{
+        height: 40px;
+        width: 40px;
+        margin-top: 80px;
+    }
+    .six{
+        height: 40px;
+        width: 40px;
     }
 `
