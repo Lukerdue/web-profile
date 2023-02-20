ef2e9970540b8b0fd8cccece369e91e228ed45fe
Jamie Jenks <jamielynjenks@gmail.com>
2023-02-19 20:48:05 -0700

    put in place the content for the contact page

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -2,6 +2,28 @@ import React from 'react';
  function Contact(){
     return(<div>
         <h1>Contact me!</h1>
+        <div className="icons-wrapper">
+            <div className="method">
+                <a href="https://linkedin.com/in/jamiejenksdev">
+                    <img src={require('../img/linkedin.png')} alt="linkedin" />
+                </a>
+            </div>
+            <div className="method">
+                <a href="https://github.com/Lukerdue">
+                    <img src={require("../img/github.png")} alt="Github" />
+                </a>
+            </div>
+            <div className="method">
+                <a href="mailto:jamielynjenks@gmail.com">
+                    <img src={require("../img/gmail.png")} alt="e-mail" />
+                </a>
+            </div>
+        </div>
+        <div className="txt">
+            <p>Jamie Jenks</p>
+            <p><a href="tel:2084212696">+12084212696</a></p>
+            <p><a href="mailto:jamielynjenks@gmail.com">jamielynjenks@gmail.com</a></p>
+        </div>
     </div>)
  }
  export default Contact
